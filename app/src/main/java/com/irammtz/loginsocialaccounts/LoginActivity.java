001de20a2a1826648cbe3a387569209264cdd6ac
Iram Martinez <irammtz01@gmail.com>
2018-06-02 18:11:25 -0500

    Login with Facebook

--- app/src/main/java/com/irammtz/loginsocialaccounts/LoginActivity.java
+++ app/src/main/java/com/irammtz/loginsocialaccounts/LoginActivity.java
@@ -6,20 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.Login;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.facebook.FacebookSdk;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    //Google
     private GoogleApiClient googleApiClient;
     public static final int SIGN_IN_CODE_GOOGLE=157;
+    //Facebook
+    CallbackManager callbackManager = CallbackManager.Factory.create();
+    LoginButton loginButtonFacebook;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Google
         SignInButton signInButtonGoogle=findViewById(R.id.login_button_Google);
         GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleApiClient=new GoogleApiClient.Builder(this)
@@ -40,11 +56,41 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 startActivityForResult(intent, SIGN_IN_CODE_GOOGLE);
             }
         });
+        //Facebook
+        loginButtonFacebook = (LoginButton) findViewById(R.id.login_facebook_btn);
+        loginButtonFacebook.setReadPermissions("email", "user_birthday", "public_profile");
 
+
+
+        // Callback registration
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        goToProfile();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        Toast.makeText(LoginActivity.this,"The login was canceled",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        Toast.makeText(LoginActivity.this,"There was an error in the login",Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode==SIGN_IN_CODE_GOOGLE) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==SIGN_IN_CODE_GOOGLE) {//Google
             GoogleSignInResult result=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
         }
--- app/src/main/java/com/irammtz/loginsocialaccounts/ProfileActivity.java
+++ app/src/main/java/com/irammtz/loginsocialaccounts/ProfileActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -21,11 +26,20 @@ import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 public class ProfileActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     ImageView imgUser;
     TextView tvName, tvEmail;
-
+    //Gooogle
     private GoogleApiClient googleApiClient;
+    //Facebook
+    AccessToken accessToken = AccessToken.getCurrentAccessToken();
+    boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +49,7 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
         imgUser=findViewById(R.id.imgUser);
         tvName=findViewById(R.id.tvName);
         tvEmail=findViewById(R.id.tvEmail);
+        //Google
         GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleApiClient=new GoogleApiClient.Builder(this)
                 .enableAutoManage(this, this)
@@ -62,7 +77,11 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
                 });
             }
         });
+
+
     }
+
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -88,10 +107,26 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
 
             Glide.with(this).load(account.getPhotoUrl()).into(imgUser);
         }else {
+            if (!isLoggedIn)
             goLogInScreen();
+            else
+                loadProfileFacebook();
         }
     }
+    public void loadProfileFacebook(){
+        GraphRequest request = GraphRequest.newMeRequest(
+                accessToken,
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        tvName.setText(object.optString("name"));
+                        Glide.with(ProfileActivity.this).load("https://graph.facebook.com/" + object.optString("id") + "/picture?width=500&height=500").into(imgUser);
 
+                    }
+                });
+        request.executeAsync();
+        tvEmail.setText("");
+    }
     private void goLogInScreen() {
         Intent intent=new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
